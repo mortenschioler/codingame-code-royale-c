acddb7cbf6e78850db789f6baa78c805b1b3ddb3
Morten Emmanuel Schiøler <morten.schioler@visma.com>
2021-02-22 17:32:14 +0100

    Generate barracks building (with bug)

--- program.c
+++ program.c
@@ -3,10 +3,19 @@
 #include <string.h>
 #include <stdbool.h>
 #include <math.h>
+#include <sys/time.h>
 
 #define NUM_SITES_MAX 50
 #define OWNER_FRIENDLY 0
 #define NONE -1
+#define STRUCTURE_TYPE_TOWER 1
+#define STRUCTURE_TYPE_BARRACKS 2
+#define CREEP_TYPE_KNIGHT 0
+#define CREEP_TYPE_ARCHER 1
+#define CREEP_TYPE_GIANT 2
+#define CMD_TYPE_WAIT 0
+#define CMD_TYPE_MOVE 1
+#define CMD_TYPE_BUILD 2
 
 struct site_static;
 struct game_static;
@@ -81,45 +90,19 @@ double distance(int x0, int y0, int x1, int x2);
 
 double game_value(struct game *);
 void simulate(struct game * g, char * cmd);
-void candidates(struct game *, char ** cands);
-
-
+void candidates(const struct game *, char * cands);
 
 struct game_static gs;
 
-int main()
-{
+long ms_epoch () {
+    struct timeval tv;
 
-	load_game_static(&gs);
-	print_game_static(&gs);
+    gettimeofday(&tv, NULL);
 
-	// game loop
-	while (1) {
-		struct game game;
-		load_game(&game);
-
-		char * best_strategy = "WAIT\nTRAIN\n";
-		char * cands[1000];
-		candidates(&game, cands);
-		double value = -1000000;
-		double val;
-		for (char ** c = cands; **c != '\0'; c++) {
-			struct game sim;
-			copy_game (&game, &sim);
-			simulate(&sim, *c);
-			val = game_value(&sim);
-			if (val > value) {
-				best_strategy = *c;
-				value = val;
-			}
-		}
-		printf("%s", best_strategy);
-
-		print_game(&game);
-		free_game(&game);
-	}
-
-	return 0;
+    unsigned long long millisecondsSinceEpoch =
+    (unsigned long long)(tv.tv_sec) * 1000 +
+    (unsigned long long)(tv.tv_usec) / 1000;
+    return millisecondsSinceEpoch;
 }
 
 void load_game_static (struct game_static *gs) {
@@ -223,6 +206,16 @@ double distance (int x0, int y0, int x1, int y1) {
 	return sqrt(square(x1 - x0) + square(y1 - y0));
 }
 
+void copy_game (struct game * from, struct game * to) {
+	to->gold = from->gold;
+    to->num_units = from->num_units;
+    to->touched_site = from->touched_site;
+    to->sites = malloc(gs.num_sites * sizeof(struct site));
+    for (int i=0;i<gs.num_sites;i++) to->sites[i] = from->sites[i];
+    to->units = malloc(from->num_units * sizeof(struct unit));
+    for (int i=0;i<from->num_units;i++) to->units[i] = from->units[i];
+}
+
 int count_sites (struct game * g, int (*f)(struct site)) {
 	int acc = 0;
 	for (int i = 0; i < gs.num_sites; i++) {
@@ -235,30 +228,22 @@ int site_weight (struct site s) {
 	return (s.structure_type != NONE) * (s.owner == OWNER_FRIENDLY);
 }
 
-char * as_word (enum barracks_type type) {
-	switch (type) {
-		case KNIGHT: return "KNIGHT";
-		case ARCHER: return "ARCHER";
-		case GIANT: return "GIANT";
-	}
-}
-
-enum barracks_type find_barracks_type (char * cmd) {
-	if (strstr(cmd, "KNIGHT")) return KNIGHT;
-	if (strstr(cmd, "ARCHER")) return ARCHER;
-	// FIXME bad fallthrough (maybe get rid of enum)
-	return GIANT;
-}
-
-void copy_game (struct game * from, struct game * to) {
-	*to = *from;
-}
-
 double game_value(struct game * g) {
 	return count_sites(g, site_weight);
 }
 
-int parse_site_id (char * s) {
+int cmd_type(char * cmd) {
+    switch (*cmd) {
+        case 'W': return CMD_TYPE_WAIT;
+        case 'M': return CMD_TYPE_MOVE;
+        case 'B': return CMD_TYPE_BUILD;
+        default: return NONE;
+    }
+}
+
+int parse_decimal (char * s) {
+    // supports string of one or two digits. The string needs not be
+    // null-terminated.
 	int n1, n2;
 	n1 = *s - '0';
 	n2 = *(++s);
@@ -269,22 +254,109 @@ int parse_site_id (char * s) {
 	}
 }
 
+char dig(int n) {
+    // n must be within range [0-9].
+    return n + '0';
+}
+
 void simulate(struct game * g, char * cmd) {
-	if (strncmp ("BUILD ", cmd, 6) == 0) {
-		int site_id = parse_site_id(cmd + 6 * sizeof (char));
-		g->sites[site_id].structure_type = 1;
+    // hack: Only value build commands and assume they
+    // are immediately constructed
+	if (cmd_type(cmd) == CMD_TYPE_BUILD) {
+		int site_id = parse_decimal(cmd + 6 * sizeof (char));
+		g->sites[site_id].structure_type = STRUCTURE_TYPE_BARRACKS;
+        g->sites[site_id].param_2 = CREEP_TYPE_KNIGHT;
+        g->sites[site_id].owner = OWNER_FRIENDLY;
 	}
 }
 
-void candidates(struct game * g, char ** cands) {
-	int i = 0;
-	char * str = "BUILD 0 BARRACKS-KNIGHT\nTRAIN\n";
-	for (int site_id = 0; site_id < 10; site_id++) {
-		str[6] = site_id - '0';
-		cands[i++] = str;
+char * base_build_cmd_str (int structure_type, int creep_type) {
+    switch (structure_type) {
+        case (STRUCTURE_TYPE_BARRACKS):
+        switch (creep_type) {
+            case CREEP_TYPE_KNIGHT: return "BUILD 00 BARRACKS-KNIGHT\n";
+            case CREEP_TYPE_ARCHER: return "BUILD 00 BARRACKS-ARCHER\n";
+            case CREEP_TYPE_GIANT: return "BUILD 00 BARRACKS-GIANT\n";
+            default: exit(1);
+        }
+        case (STRUCTURE_TYPE_TOWER): return "BUILD 00 TOWER\n";
+        default: exit(1);
+    }
+}
+
+void format_decimal (char * dest, int n) {
+    // Takes a pointer to two consecutive characters
+    // and overwrites them with the ten-part and the one-part of n
+    int ones, tens;
+    tens = n / 10;
+    ones = n % 10;
+    dest[0] = dig(tens);
+    dest[1] = dig(ones);
+}
+
+void build_cmd (char * dest, int site_id, int structure_type, int creep_type) {
+    char * base_str = base_build_cmd_str(structure_type, creep_type);
+    strcpy(dest, base_str);
+    format_decimal(&dest[6], site_id);
+}
+
+void candidates(const struct game * g, char * cands) {
+    char * cmd = cands;
+    // For each site, build each building
+    for (int i = 0; i < gs.num_sites; i++) {
+        build_cmd(cmd, i, STRUCTURE_TYPE_BARRACKS, CREEP_TYPE_KNIGHT);
+        strcat(cmd, "TRAIN\n");
+        // move to end of string
+        while(*(cmd++));
+    }
+}
+
+int main()
+{
+
+	load_game_static(&gs);
+	print_game_static(&gs);
+
+    char cands[10000];
+
+	// game loop
+	while (1) {
+        long t1,t2;
+
+		struct game game;
+		load_game(&game);
+        // print_game(&game);
+
+        t1 = ms_epoch();
+
+		char * best_strategy = "WAIT\nTRAIN\n";
+        double best_value = game_value(&game);
+        
+        candidates(&game, cands);
+        
+		for (char * s = cands; *s != '\0'; ) {
+            struct game game2;
+            copy_game(&game, &game2);
+            simulate(&game2, s);
+            double value = game_value(&game2);
+            fprintf(stderr, "Simulated value of %s is %f.\n", s, value);
+
+            if(value >= best_value) {
+                fprintf(stderr, "New best strategy found.\n");
+                best_value = value;
+                best_strategy = s;
+            }
+            free_game(&game2);
+            while(*s++ != '\0');
+        }
+
+        t2 = ms_epoch();
+
+        fprintf(stderr, "Elapsed time for game round: %d ms\n", t2-t1);
+		fprintf(stdout,"%s", best_strategy);
+
+		free_game(&game);
 	}
 
-	cands[0] = str;
-	cands[1] = "";
-	cands[0] = "";
+	return 0;
 }
